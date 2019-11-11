85fb1c9ba1295ff54b5cd2b3e2ca500d930e83f2
Brad Flood <bflood@keyholesoftware.com>
2019-11-11 15:10:00 -0600

    polishing

--- elastic-beanstalk/main.tf
+++ elastic-beanstalk/main.tf
@@ -18,7 +18,6 @@ module "vpc" {
   name       = var.name
   attributes = var.attributes
   tags       = var.tags
-  delimiter  = var.delimiter
   cidr_block = "172.16.0.0/16"
 }
 
@@ -30,81 +29,50 @@ module "subnets" {
   name                 = var.name
   attributes           = var.attributes
   tags                 = var.tags
-  delimiter            = var.delimiter
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
   cidr_block           = module.vpc.vpc_cidr_block
-  nat_gateway_enabled  = true
+  nat_gateway_enabled  = var.nat_gateway_enabled
   nat_instance_enabled = false
 }
 
 module "elastic_beanstalk_application" {
-  source      = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-application.git?ref=tags/0.4.0"
-  namespace   = var.namespace
-  stage       = var.stage
-  # name        = var.application_name
-  name        = var.name
-  attributes  = var.attributes
-  tags        = var.tags
-  delimiter   = var.delimiter
+  source    = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-application.git?ref=tags/0.4.0"
+  namespace = var.namespace
+  stage     = var.stage
+  name       = var.name
+  attributes = var.attributes
+  tags       = var.tags
   description = var.application_description
 }
 
 module "elastic_beanstalk_environment" {
-  source                     = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=tags/0.17.0"
-  namespace                  = var.namespace
-  stage                      = var.stage
-  # name                       = var.environment_name
-  name                       = var.name  
-  attributes                 = var.attributes
-  tags                       = var.tags
-  delimiter                  = var.delimiter
-  description                = var.environment_description
-  region                     = var.aws_region
-  availability_zone_selector = var.availability_zone_selector
-  dns_zone_id                = var.dns_zone_id
-
-  wait_for_ready_timeout             = var.wait_for_ready_timeout
-  elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
-  environment_type                   = var.environment_type
-  loadbalancer_type                  = var.loadbalancer_type
-  elb_scheme                         = var.elb_scheme
-  tier                               = var.tier
-  version_label                      = var.version_label
-  force_destroy                      = var.force_destroy
-
-  instance_type    = var.instance_type
-  root_volume_size = var.root_volume_size
-  root_volume_type = var.root_volume_type
-
-  autoscale_min             = var.autoscale_min
-  autoscale_max             = var.autoscale_max
-  autoscale_measure_name    = var.autoscale_measure_name
-  autoscale_statistic       = var.autoscale_statistic
-  autoscale_unit            = var.autoscale_unit
-  autoscale_lower_bound     = var.autoscale_lower_bound
-  autoscale_lower_increment = var.autoscale_lower_increment
-  autoscale_upper_bound     = var.autoscale_upper_bound
-  autoscale_upper_increment = var.autoscale_upper_increment
-
-  vpc_id                  = module.vpc.vpc_id
-  loadbalancer_subnets    = module.subnets.public_subnet_ids
-  application_subnets     = module.subnets.private_subnet_ids
-  allowed_security_groups = [module.vpc.vpc_default_security_group_id]
-
-  rolling_update_enabled  = var.rolling_update_enabled
-  rolling_update_type     = var.rolling_update_type
-  updating_min_in_service = var.updating_min_in_service
-  updating_max_batch      = var.updating_max_batch
-
-  healthcheck_url  = var.healthcheck_url
-  application_port = var.application_port
-
-  // https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html
-  // https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
-  solution_stack_name = var.solution_stack_name
+  source     = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=tags/0.17.0"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = var.name
+  attributes = var.attributes
+  tags       = var.tags
+  region     = var.aws_region
 
   additional_settings = var.additional_settings
-  env_vars            = var.env_vars
+  # application_port = var.application_port # use default
+  application_subnets                = module.subnets.private_subnet_ids
+  allowed_security_groups            = [module.vpc.vpc_default_security_group_id]
+  description                        = var.environment_description
+  dns_zone_id                        = var.dns_zone_id
+  elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
+  enable_stream_logs                 = var.enable_stream_logs
+  env_vars                           = var.env_vars
+  environment_type                   = var.environment_type
+  healthcheck_url                    = var.healthcheck_url
+  instance_type                      = var.instance_type
+  loadbalancer_certificate_arn       = var.loadbalancer_certificate_arn
+  loadbalancer_subnets               = module.subnets.public_subnet_ids
+  loadbalancer_type                  = var.loadbalancer_type
+  logs_retention_in_days             = var.logs_retention_in_days
+  rolling_update_type                = var.rolling_update_type
+  solution_stack_name                = var.solution_stack_name
+  vpc_id                             = module.vpc.vpc_id
 }
 
--- elastic-beanstalk/variables.tf
+++ elastic-beanstalk/variables.tf
@@ -1,3 +1,4 @@
+# AWS 
 variable "aws_region" {
   type        = string
   description = "AWS region"
@@ -8,11 +9,7 @@ variable "aws_profile" {
   default = "dbp-admin"
 }
 
-variable "availability_zones" {
-  type        = list(string)
-  description = "List of availability zones"
-}
-
+# administrative, to match cloudposse label
 variable "namespace" {
   type        = string
   description = "Namespace, which could be your organization name, e.g. 'eg' or 'cp'"
@@ -28,183 +25,14 @@ variable "name" {
   description = "Solution name, e.g. 'app' or 'cluster'"
 }
 
-variable "delimiter" {
-  type        = string
-  default     = "-"
-  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
-}
-
-variable "attributes" {
-  type        = list(string)
-  default     = []
-  description = "Additional attributes (e.g. `1`)"
-}
-
 variable "tags" {
   type        = map(string)
   default     = {}
   description = "Additional tags (e.g. `map('BusinessUnit`,`XYZ`)"
 }
 
-# variable "application_name" {
-#   type        = string
-#   description = "Application name, e.g. 'app' or 'cluster'"
-# }
 
-variable "application_description" {
-  type        = string
-  description = "Short description of the Elastic Beanstalk Application"
-}
-
-# variable "environment_name" {
-#   type        = string
-#   description = "Environment name"
-# }
-variable "environment_description" {
-  type        = string
-  description = "Short description of the Elastic Beanstalk Environment"
-}
-
-variable "environment_type" {
-  type        = string
-  description = "Environment type, e.g. 'LoadBalanced' or 'SingleInstance'.  If setting to 'SingleInstance', `rolling_update_type` must be set to 'Time', `updating_min_in_service` must be set to 0, and `loadbalancer_subnets` will be unused (it applies to the ELB, which does not exist in SingleInstance environments)"
-}
-
-variable "loadbalancer_type" {
-  type        = string
-  description = "Load Balancer type, e.g. 'application' or 'classic'"
-}
-
-variable "dns_zone_id" {
-  type        = string
-  description = "Route53 parent zone ID. The module will create sub-domain DNS record in the parent zone for the EB environment"
-}
-
-variable "availability_zone_selector" {
-  type        = string
-  description = "Availability Zone selector"
-}
-
-variable "instance_type" {
-  type        = string
-  description = "Instances type"
-}
-
-variable "autoscale_min" {
-  type        = number
-  description = "Minumum instances to launch"
-}
-
-variable "autoscale_max" {
-  type        = number
-  description = "Maximum instances to launch"
-}
-
-variable "solution_stack_name" {
-  type        = string
-  description = "Elastic Beanstalk stack, e.g. Docker, Go, Node, Java, IIS. For more info, see https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html"
-}
-
-variable "wait_for_ready_timeout" {
-  type        = string
-  description = "The maximum duration to wait for the Elastic Beanstalk Environment to be in a ready state before timing out"
-}
-
-variable "tier" {
-  type        = string
-  description = "Elastic Beanstalk Environment tier, e.g. 'WebServer' or 'Worker'"
-}
-
-variable "version_label" {
-  type        = string
-  description = "Elastic Beanstalk Application version to deploy"
-}
-
-variable "force_destroy" {
-  type        = bool
-  description = "Force destroy the S3 bucket for load balancer logs"
-}
-
-variable "rolling_update_enabled" {
-  type        = bool
-  description = "Whether to enable rolling update"
-}
-
-variable "rolling_update_type" {
-  type        = string
-  description = "`Health` or `Immutable`. Set it to `Immutable` to apply the configuration change to a fresh group of instances"
-}
-
-variable "updating_min_in_service" {
-  type        = number
-  description = "Minimum number of instances in service during update"
-}
-
-variable "updating_max_batch" {
-  type        = number
-  description = "Maximum number of instances to update at once"
-}
-
-variable "healthcheck_url" {
-  type        = string
-  description = "Application Health Check URL. Elastic Beanstalk will call this URL to check the health of the application running on EC2 instances"
-}
-
-variable "application_port" {
-  type        = number
-  description = "Port application is listening on"
-}
-
-variable "root_volume_size" {
-  type        = number
-  description = "The size of the EBS root volume"
-}
-
-variable "root_volume_type" {
-  type        = string
-  description = "The type of the EBS root volume"
-}
-
-variable "autoscale_measure_name" {
-  type        = string
-  description = "Metric used for your Auto Scaling trigger"
-}
-
-variable "autoscale_statistic" {
-  type        = string
-  description = "Statistic the trigger should use, such as Average"
-}
-
-variable "autoscale_unit" {
-  type        = string
-  description = "Unit for the trigger measurement, such as Bytes"
-}
-
-variable "autoscale_lower_bound" {
-  type        = number
-  description = "Minimum level of autoscale metric to remove an instance"
-}
-
-variable "autoscale_lower_increment" {
-  type        = number
-  description = "How many Amazon EC2 instances to remove when performing a scaling activity."
-}
-
-variable "autoscale_upper_bound" {
-  type        = number
-  description = "Maximum level of autoscale metric to add an instance"
-}
-
-variable "autoscale_upper_increment" {
-  type        = number
-  description = "How many Amazon EC2 instances to add when performing a scaling activity"
-}
-
-variable "elb_scheme" {
-  type        = string
-  description = "Specify `internal` if you want to create an internal load balancer in your Amazon VPC so that your Elastic Beanstalk application cannot be accessed from outside your Amazon VPC"
-}
-
+# module-specific, sorted alphabetically 
 variable "additional_settings" {
   type = list(object({
     namespace = string
@@ -216,8 +44,151 @@ variable "additional_settings" {
   default     = []
 }
 
+variable "application_description" {
+  type        = string
+  description = "Short description of the Elastic Beanstalk Application"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
+variable "dns_zone_id" {
+  type        = string
+  description = "Route53 parent zone ID. The module will create sub-domain DNS record in the parent zone for the EB environment"
+}
+variable "enable_stream_logs" {
+  #Cloudposse default: false
+  type        = bool
+  default     = true
+  description = "Whether to create groups in CloudWatch Logs for proxy and deployment logs, and stream logs from each instance in your environment"
+}
+
 variable "env_vars" {
   type        = map(string)
   default     = {}
   description = "Map of custom ENV variables to be provided to the application running on Elastic Beanstalk, e.g. env_vars = { DB_USER = 'admin' DB_PASS = 'xxxxxx' }"
 }
+
+variable "environment_description" {
+  type        = string
+  description = "Short description of the Elastic Beanstalk Environment"
+}
+
+variable "environment_type" {
+  type        = string
+  description = "Environment type, e.g. 'LoadBalanced' or 'SingleInstance'.  If setting to 'SingleInstance', `rolling_update_type` must be set to 'Time', `updating_min_in_service` must be set to 0, and `loadbalancer_subnets` will be unused (it applies to the ELB, which does not exist in SingleInstance environments)"
+  default     = "LoadBalanced"
+}
+
+variable "health_streaming_enabled" {
+  #Cloudposse default: false
+  type        = bool
+  default     = true
+  description = "For environments with enhanced health reporting enabled, whether to create a group in CloudWatch Logs for environment health and archive Elastic Beanstalk environment health data. For information about enabling enhanced health, see aws:elasticbeanstalk:healthreporting:system."
+}
+
+variable "healthcheck_url" {
+  #Cloudposse default: /healthcheck
+  type        = string
+  description = "Application Health Check URL. Elastic Beanstalk will call this URL to check the health of the application running on EC2 instances"
+  default     = "/"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instances type"
+}
+
+variable "loadbalancer_type" {
+  # cloudposse default is classic
+  type        = string
+  description = "Load Balancer type, e.g. 'application' or 'classic'"
+  default     = "application"
+}
+variable "loadbalancer_certificate_arn" {
+  type        = string
+  default     = ""
+  description = "Load Balancer SSL certificate ARN. The certificate must be present in AWS Certificate Manager"
+}
+variable "logs_retention_in_days" {
+  #Cloudposse default: 7
+  type        = number
+  default     = 60
+  description = "The number of days to keep log events before they expire."
+}
+variable "nat_gateway_enabled" {
+  # cloudposse default is true  
+  type        = bool
+  description = "Flag to enable/disable NAT Gateways to allow servers in the private subnets to access the Internet"
+  default     = false
+}
+
+variable "rolling_update_type" {
+  # cloudposse default is Health  
+  type        = string
+  description = "`Health` or `Immutable`. Set it to `Immutable` to apply the configuration change to a fresh group of instances"
+  default     = "Immutable"
+}
+
+variable "solution_stack_name" {
+  type        = string
+  description = "Elastic Beanstalk stack, e.g. Docker, Go, Node, Java, IIS. For more info, see https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html"
+}
+
+# variable "version_label" {
+#   type        = string
+#   description = "Elastic Beanstalk Application version to deploy"
+# }
+
+# these may come back if Jon wants NetworkOut
+# variable "autoscale_measure_name" {
+#   type        = string
+#   description = "Metric used for your Auto Scaling trigger"
+# }
+
+# variable "autoscale_statistic" {
+#   type        = string
+#   description = "Statistic the trigger should use, such as Average"
+# }
+
+# variable "autoscale_unit" {
+#   type        = string
+#   description = "Unit for the trigger measurement, such as Bytes"
+# }
+
+# variable "autoscale_lower_bound" {
+#   type        = number
+#   description = "Minimum level of autoscale metric to remove an instance"
+# }
+
+# variable "autoscale_lower_increment" {
+#   type        = number
+#   description = "How many Amazon EC2 instances to remove when performing a scaling activity."
+# }
+
+# variable "autoscale_upper_bound" {
+#   type        = number
+#   description = "Maximum level of autoscale metric to add an instance"
+# }
+
+# variable "autoscale_upper_increment" {
+#   type        = number
+#   description = "How many Amazon EC2 instances to add when performing a scaling activity"
+# }
+
+
+
+# variable "elb_scheme" {
+#   type        = string
+#   description = "Specify `internal` if you want to create an internal load balancer in your Amazon VPC so that your Elastic Beanstalk application cannot be accessed from outside your Amazon VPC"
+# }
+
+
