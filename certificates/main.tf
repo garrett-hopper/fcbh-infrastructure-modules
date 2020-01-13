de08cc5ce128fbe4311fdc2a7005d63ac0b0b63c
Brad Flood <bflood@keyholesoftware.com>
2020-01-13 08:07:59 -0600

    initial commit certificates

---
+++ certificates/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+
+  # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
+  required_version = ">= 0.12.0"
+}
+
+
+module "acm_request_certificate" {
+  source                    = "git::https://github.com/cloudposse/terraform-aws-acm-request-certificate.git?ref=master"
+  domain_name               = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+  tags                      = var.tags
+}
---
+++ certificates/variables.tf
@@ -0,0 +1,25 @@
+# This module can be applied in multiple regions
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+# specify a different AWS profile to provide different access keys
+variable "aws_profile" {
+  type    = string
+  default = "bibleis-admin"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "A domain name for which the certificate should be issued"
+}
+variable "subject_alternative_names" {
+  type        = list(string)
+  default     = []
+  description = "A list of domains that should be SANs in the issued certificate"
+}
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. map('BusinessUnit`,`XYZ`)"
+}
