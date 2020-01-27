d5e478622c33a21d6af3f846b48cf9ee31b1b690
Brad Flood <bflood@keyholesoftware.com>
2020-01-26 19:37:34 -0600

    remove ami_id, polish

--- bastion/outputs.tf
+++ bastion/outputs.tf
@@ -1,11 +1,11 @@
-output "security_group_id"  { 
-    value = "${module.security_group.this_security_group_id}" 
-}
 
 output "public_ip"  { 
     value = module.host.public_ip
 }
 output "public_dns"  { 
     value = module.host.public_dns
+}
+output "security_group_id"  { 
+    value = "${module.security_group.this_security_group_id}" 
 }
 
--- bastion/variables.tf
+++ bastion/variables.tf
@@ -13,24 +13,23 @@ variable "security_group_name" {
   type    = string
   default = "Bastion Security Group"
 }
-variable "vpc_id" {
 
+variable "vpc_id" {
 }
+
 variable "control_cidr" {
   type = list(string)
 }
 variable "host_name" {
   default = "Bastion Host"
 }
-variable "ami_id" {
 
-}
 variable "key_name" {
-
 }
+
 variable "instance_type" {
   default = "t3.small"
 }
-variable "subnet_id" {
 
+variable "subnet_id" {
 }
