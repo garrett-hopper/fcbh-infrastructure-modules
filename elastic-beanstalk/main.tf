a081b7165cb425f6747529dfdb481167262f066e
Brad Flood <bflood@keyholesoftware.com>
2020-02-08 13:48:23 -0600

    narrow focus on modeling beanstalk

--- elastic-beanstalk/main.tf
+++ elastic-beanstalk/main.tf
@@ -11,68 +11,12 @@ terraform {
   required_version = ">= 0.12.0"
 }
 
-module "vpc" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.8.1"
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
-  attributes = var.attributes
-  tags       = var.tags
-  cidr_block = "172.16.0.0/16"
+# the initial configuration will create the resources necessary for the eb command line to create the beanstalk application and enviroment
+# the beanstalk configuration will initially deploy into the default VPC
+
+module "acm_request_certificate" {
+  source                    = "git::https://github.com/cloudposse/terraform-aws-acm-request-certificate.git?ref=master"
+  domain_name               = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+  tags                      = var.tags
 }
-
-module "subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.16.1"
-  availability_zones   = var.availability_zones
-  namespace            = var.namespace
-  stage                = var.stage
-  name                 = var.name
-  attributes           = var.attributes
-  tags                 = var.tags
-  vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
-  nat_gateway_enabled  = var.nat_gateway_enabled
-  nat_instance_enabled = false
-}
-
-module "elastic_beanstalk_application" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-application.git?ref=tags/0.4.0"
-  namespace = var.namespace
-  stage     = var.stage
-  name       = var.name
-  attributes = var.attributes
-  tags       = var.tags
-  description = var.application_description
-}
-
-module "elastic_beanstalk_environment" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=tags/0.17.0"
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
-  attributes = var.attributes
-  tags       = var.tags
-  region     = var.aws_region
-
-  additional_settings = var.additional_settings
-  # application_port = var.application_port # use default
-  application_subnets                = module.subnets.private_subnet_ids
-  allowed_security_groups            = [module.vpc.vpc_default_security_group_id]
-  description                        = var.environment_description
-  dns_zone_id                        = var.dns_zone_id
-  elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
-  enable_stream_logs                 = var.enable_stream_logs
-  env_vars                           = var.env_vars
-  environment_type                   = var.environment_type
-  healthcheck_url                    = var.healthcheck_url
-  instance_type                      = var.instance_type
-  loadbalancer_certificate_arn       = var.loadbalancer_certificate_arn
-  loadbalancer_subnets               = module.subnets.public_subnet_ids
-  loadbalancer_type                  = var.loadbalancer_type
-  logs_retention_in_days             = var.logs_retention_in_days
-  rolling_update_type                = var.rolling_update_type
-  solution_stack_name                = var.solution_stack_name
-  vpc_id                             = module.vpc.vpc_id
-}
-
