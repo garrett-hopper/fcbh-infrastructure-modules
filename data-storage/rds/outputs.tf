503480b34e8a7c48428ce5bab50f5c15a109bc09
Brad Flood <bflood@keyholesoftware.com>
2020-02-17 16:13:54 -0600

    add reader endpoint for rds output

--- data-storage/rds/outputs.tf
+++ data-storage/rds/outputs.tf
@@ -2,3 +2,8 @@ output "endpoint" {
   value       = module.rds_cluster_aurora_mysql.endpoint
   description = "The DNS address of the RDS instance"
 }
+
+output "reader_endpoint" {
+  value       = module.rds_cluster_aurora_mysql.reader_endpoint
+  description = "A read-only endpoint for the Aurora cluster, automatically load-balanced across replicas"
+}
