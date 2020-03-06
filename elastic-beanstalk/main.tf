2e15878e65b082582380f063928076622de6707b
Brad Flood <bflood@keyholesoftware.com>
2020-03-06 13:38:14 -0600

    polish

--- elastic-beanstalk/main.tf
+++ elastic-beanstalk/main.tf
@@ -8,10 +8,6 @@ terraform {
   required_version = ">= 0.12.0"
 }
 
-# data "aws_availability_zones" "all" {
-#   state = "available"
-# }
-
 module "elastic_beanstalk_application" {
   source      = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-application.git?ref=tags/0.4.0"
   namespace   = var.namespace
@@ -32,6 +28,7 @@ module "elastic_beanstalk_environment" {
   region     = var.aws_region
 
   additional_settings                = var.additional_settings
+  vpc_id                             = var.vpc_id
   loadbalancer_subnets               = var.public_subnets
   application_subnets                = var.private_subnets
   allowed_security_groups            = var.allowed_security_groups
@@ -45,10 +42,10 @@ module "elastic_beanstalk_environment" {
   healthcheck_url                    = var.healthcheck_url
   instance_type                      = var.instance_type
   loadbalancer_certificate_arn       = var.loadbalancer_certificate_arn
+  loadbalancer_ssl_policy            = "ELBSecurityPolicy-2016-08"
   loadbalancer_type                  = var.loadbalancer_type
   logs_retention_in_days             = var.logs_retention_in_days
   rolling_update_type                = var.rolling_update_type
   solution_stack_name                = var.solution_stack_name
-  vpc_id                             = var.vpc_id
   keypair                            = var.keypair
 }
