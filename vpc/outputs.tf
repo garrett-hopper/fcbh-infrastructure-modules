8c934e1c6f39956a352fad851104b5ac83210f43
Brad Flood <bflood@keyholesoftware.com>
2020-03-06 22:04:42 -0600

    add bastion_subnet_id (intermittent issues)

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -16,10 +16,10 @@ output "private_subnet_ids" {
   description = "IDs of the created private subnets"
   value       = module.subnets.private_subnet_ids
 }
-# output "bastion_subnet_id" {
-#   description = "Subnet Id containing the bastion host"
-#   value       = module.subnets.public_subnet_ids[0]
-# }
+output "bastion_subnet_id" {
+  description = "Subnet Id containing the bastion host"
+  value       = module.subnets.public_subnet_ids[0]
+}
 output "availability_zones" {
   description = "List of Availability Zones where subnets were created"
   value       = module.subnets.availability_zones
