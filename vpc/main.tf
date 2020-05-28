97368ea8c7518402a7e6b2d0084efad6b3fc7a1e
Brad Flood <bflood@keyholesoftware.com>
2020-05-28 16:40:12 -0500

    be more resilient on terraform and aws versions

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,12 +1,12 @@
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
-  required_version = "= 0.12.25"
+  required_version = "~> 0.12.25"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
     aws = {
-      version = "= 2.63.0"
+      version = "= ~> 2.63.0"
     }
   }
 }
