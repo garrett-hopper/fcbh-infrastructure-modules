1cfbf024fc4136a621f1f26a2c1f2158237f4232
Brad Flood <bflood@keyholesoftware.com>
2020-06-21 19:29:43 -0500

    bump version

--- waf/main.tf
+++ waf/main.tf
@@ -1,13 +1,62 @@
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
-  required_version = "~> 0.12.25"
+  required_version = "~> 0.12"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
     aws = {
-      version = "~> 2.63.0"
+      version = "~> 2.67"
     }
   }
 }
+
+# resource "aws_wafv2_web_acl" "rate-limit" {
+#   name        = "rate-based-example"
+#   description = "rate limit access"
+#   scope       = "REGIONAL"
+
+#   default_action {
+#     block {}
+#   }
+
+#   rule {
+#     name     = "rule-1"
+#     priority = 1
+
+#     action {
+#       count {}
+#     }
+
+#     statement {
+#       rate_based_statement {
+#         limit              = 10000
+#         aggregate_key_type = "IP"
+
+#         scope_down_statement {
+#           geo_match_statement {
+#             country_codes = ["US", "NL"]
+#           }
+#         }
+#       }
+#     }
+
+#     visibility_config {
+#       cloudwatch_metrics_enabled = false
+#       metric_name                = "friendly-rule-metric-name"
+#       sampled_requests_enabled   = false
+#     }
+#   }
+
+#   tags = {
+#     Tag1 = "Value1"
+#     Tag2 = "Value2"
+#   }
+
+#   visibility_config {
+#     cloudwatch_metrics_enabled = false
+#     metric_name                = "friendly-metric-name"
+#     sampled_requests_enabled   = false
+#   }
+# }
 
